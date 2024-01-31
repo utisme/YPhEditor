e05f6369b41620169817ebc6c108ba89b43608f6
Edgar <edgar32801@gmail.com>
2024-01-31 11:15:45 +0300

    first commit

---
+++ YPhEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YPhEditor
+//
+//  Created by Эдгар Кусков on 31.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
